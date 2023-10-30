068ee1a88ba45c6e76967101eee935390b6fdf0f
= <=>
2023-10-31 01:09:06 +0530

    tries files added

--- src/com/leetcode/java/trie/ImplementTrie208.java
+++ src/com/leetcode/java/trie/ImplementTrie208.java
@@ -6,6 +6,7 @@ public class ImplementTrie208 {
         t.insert("suhag");
         t.insert("aman");
         t.insert(" suha");
+        t.insert("ankit");
         System.out.println(t.search("suhag"));
         System.out.println(t.startsWith("suh"));
         System.out.println(t.getWords());
